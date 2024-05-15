299b1723b408dbc1f730857a2e5d85617b5fcdd0
wusara87 <wusara87@csu.fullerton.edu>
2024-05-15 13:29:16 -0700

    updated

--- sara_portfolio/src/pages/Contact.jsx
+++ sara_portfolio/src/pages/Contact.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import Style from './page.module.css';
+import Style from "./grid.module.css";
+import style from "./card.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 export default function ContactPage() {
     return (
-        <div>
+        <div className={Style.container}>
             <h1>MY CONTACT INFO</h1>
             <p>Email: wusara87@csu.fullerton.edu</p>
             <p>Phone: +1 (951) 427-0568</p>
-            <a href="https://www.linkedin.com/in/sara-wu-784578294" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/sara-wu-784578294" className={style.projectLink} target="_blank" rel="noopener noreferrer">LinkedIn<FontAwesomeIcon icon={faExternalLinkAlt} className={style.externalLinkIcon}/></a>
         </div>
     );
 }
