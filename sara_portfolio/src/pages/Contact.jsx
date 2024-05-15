82c74781ca9ea787388b2e0ecf48e08b3d70c953
wusara87 <wusara87@csu.fullerton.edu>
2024-05-15 13:57:58 -0700

    updated css

--- sara_portfolio/src/pages/Contact.jsx
+++ sara_portfolio/src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import Style from "./grid.module.css";
 import style from "./card.module.css";
+import Box from "./box.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
@@ -7,9 +8,11 @@ export default function ContactPage() {
     return (
         <div className={Style.container}>
             <h1>MY CONTACT INFO</h1>
-            <p>Email: wusara87@csu.fullerton.edu</p>
-            <p>Phone: +1 (951) 427-0568</p>
-            <a href="https://www.linkedin.com/in/sara-wu-784578294" className={style.projectLink} target="_blank" rel="noopener noreferrer">LinkedIn<FontAwesomeIcon icon={faExternalLinkAlt} className={style.externalLinkIcon}/></a>
+            <div className={Box.box}>
+                <p>Email: wusara87@csu.fullerton.edu</p>
+                <p>Phone: +1 (951) 427-0568</p>
+                <a href="https://www.linkedin.com/in/sara-wu-784578294" className={style.projectLink} target="_blank" rel="noopener noreferrer">LinkedIn<FontAwesomeIcon icon={faExternalLinkAlt} className={style.externalLinkIcon}/></a>
+            </div>
         </div>
     );
 }
--- sara_portfolio/src/pages/Home.jsx
+++ sara_portfolio/src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
             <div className={Style.container}>
                 <div className={Style.text}>
                     <h1 className={Style.title}>Hi, I'm Sara</h1>
-                    <p className={Style.paragraph}>I'm a senior Computer Science student passionate about software development. I'm currently pursuing a Bachelor's Degree. </p>
+                    <p className={Style.paragraph}>I'm a senior Computer Science student at CSUF, deeply passionate about software development. Currently working towards my Bachelor's Degree, I'm excited about the endless possibilities in the world of technology.</p>
                 </div>
                 <div className={Style.image}>
                     <img src="/profile.jpg" alt="Sara" />
